564fd5d5207dd1ff1c72882676da53deeb0570fb
Jory <jolewis211@gmail.com>
2022-02-03 12:21:07 -0700

    introduced a sqlite database in lieu of list to satisify assignment requirement and add scalability; data inserted to new db rather than list in token endpoint

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 from flask_api import FlaskAPI, status
 import dummyapi.token as token
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = FlaskAPI(__name__)
-app.tokens = []
+base_dir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'\
+     + os.path.join(base_dir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# I'd like to keep deployment as-is 
+# with this function users do not need to build the db from CLI
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+class ApiKey(db.Model):
+    key = db.Column(db.String(255), primary_key=True)
+
+    def __init__(self, key):
+        self.key = key
+
+    def json(self):
+        return {'key': self.key}
 
 
 @app.route("/token", methods=["GET"])
@@ -11,7 +34,10 @@ def get_token():
     Generate and store a new token.
     """
     new_token: str = token.generate_token()
-    app.tokens.append(new_token)
+    api_obj = ApiKey(new_token)
+    db.session.add(api_obj)
+    db.session.commit()
+
     return {"status": "success", "token": new_token}
 
 

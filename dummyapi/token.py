8baf5ee4c21c1afb728de96718c8011c8a15ef5c
Jory <jolewis211@gmail.com>
2022-02-03 12:16:50 -0700

    updated rdmtoken to include ascii_letters and digits; special characters and symbols can cause issues

--- dummyapi/token.py
+++ dummyapi/token.py
@@ -8,5 +8,7 @@ def generate_token() -> str:
     Generates a random token with a length that is > 8 and < 32.
     """
     length = random.randint(8, 32)
-    rdmtoken = "".join(random.choice(string.printable) for i in range(length))
+    # special characters can cause unintended behavior; could also unpack like [*l1, *l2]
+    char_list = string.ascii_letters + string.digits
+    rdmtoken = "".join(random.choice(char_list) for i in range(length))
     return f"{rdmtoken}"
